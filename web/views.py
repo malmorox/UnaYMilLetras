1c95399de89b9f995579c92c42aab040815e4a0a
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-20 19:49:19 +0100

    Cambios varios

--- web/views.py
+++ web/views.py
@@ -22,3 +22,11 @@ def detail(request, article_id):
 #def article_list(request):
     #articles = Article.objects.all()
     #return render(request, 'article_list.html', {'articles': articles})
+
+
+def contact(request):
+    article = Article.objects.get(id=article_id)
+    context = {
+        'article': article
+    }
+    return render(request, 'web/contact.html', context)
