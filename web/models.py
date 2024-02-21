f55542fe61058e2d8008eb6ca5e6a54214beb4f7
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-21 21:54:03 +0100

    Cambios varios

--- web/models.py
+++ web/models.py
@@ -19,3 +19,18 @@ class Article(models.Model):
 
     def __str__(self):  
         return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = HTMLField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.article.title}'
